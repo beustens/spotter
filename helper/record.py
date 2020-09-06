4b4047d297e476ff23da5919410a4a24595d300b
Niklas Beuster <niklas.beuster@tu-ilmenau.de>
2020-09-06 15:20:13 +0200

    added utility scripts for recording video and fluid webstreaming

---
+++ helper/record.py
@@ -0,0 +1,16 @@
+from picamera import PiCamera # to access the camera
+from datetime import datetime # for timestamp
+
+fps = 15
+camera = PiCamera(resolution=(1920, 1080), framerate=fps)
+camera.meter_mode = 'spot'
+camera.exposure_mode = 'verylong'
+
+dt = datetime.now().strftime('%Y.%m.%d-%H.%M')
+camera.start_recording(f'video_{fps}fps_{dt}.h264')
+try:
+    while True:
+        camera.wait_recording(5)
+except KeyboardInterrupt:
+    pass
+camera.stop_recording()
---
+++ helper/stream.py
@@ -0,0 +1,95 @@
+import io
+import picamera
+import socketserver
+from threading import Condition
+from http import server
+
+
+PAGE="""\
+<html>
+<head>
+<title>picamera MJPEG streaming demo</title>
+</head>
+<body>
+<h1>PiCamera MJPEG Streaming Demo</h1>
+<img src="stream.mjpg" width="100%"/>
+</body>
+</html>
+"""
+
+
+class StreamingOutput(object):
+    def __init__(self):
+        self.frame = None
+        self.buffer = io.BytesIO()
+        self.condition = Condition()
+
+    def write(self, buf):
+        if buf.startswith(b'\xff\xd8'):
+            # New frame, copy the existing buffer's content and notify all
+            # clients it's available
+            self.buffer.truncate()
+            with self.condition:
+                self.frame = self.buffer.getvalue()
+                self.condition.notify_all()
+            self.buffer.seek(0)
+        return self.buffer.write(buf)
+
+
+class StreamingHandler(server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(301)
+            self.send_header('Location', '/index.html')
+            self.end_headers()
+        elif self.path == '/index.html':
+            content = PAGE.encode('utf-8')
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Length', len(content))
+            self.end_headers()
+            self.wfile.write(content)
+        elif self.path == '/stream.mjpg':
+            self.send_response(200)
+            self.send_header('Age', 0)
+            self.send_header('Cache-Control', 'no-cache, private')
+            self.send_header('Pragma', 'no-cache')
+            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
+            self.end_headers()
+            try:
+                while True:
+                    with output.condition:
+                        output.condition.wait()
+                        frame = output.frame
+                    self.wfile.write(b'--FRAME\r\n')
+                    self.send_header('Content-Type', 'image/jpeg')
+                    self.send_header('Content-Length', len(frame))
+                    self.end_headers()
+                    self.wfile.write(frame)
+                    self.wfile.write(b'\r\n')
+            except Exception:
+                pass
+        else:
+            self.send_error(404)
+            self.end_headers()
+
+
+class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
+    allow_reuse_address = True
+    daemon_threads = True
+
+
+with picamera.PiCamera(resolution=(1920, 1080), framerate=30) as camera:
+    camera.exposure_mode = 'verylong'
+    output = StreamingOutput()
+    camera.start_recording(output, format='mjpeg')
+    try:
+        server = StreamingServer(('', 8000), StreamingHandler)
+        print(f'Started stream on http://{server.server_name}:{server.server_port}')
+        print('Press ctrl-C to stop')
+        server.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        camera.stop_recording()
+        print('Stopped stream')
