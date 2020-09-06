1bcf18e776611697c89ce5c189f7ef3293b6f343
Niklas Beuster <niklas.beuster@tu-ilmenau.de>
2020-09-06 15:45:39 +0200

    recorded videos should now be saved in the home directory

--- helper/record.py
+++ helper/record.py
@@ -7,7 +7,7 @@ camera.meter_mode = 'spot'
 camera.exposure_mode = 'verylong'
 
 dt = datetime.now().strftime('%Y.%m.%d-%H.%M')
-camera.start_recording(f'video_{fps}fps_{dt}.h264')
+camera.start_recording(f'/home/pi/video_{fps}fps_{dt}.h264')
 try:
     while True:
         camera.wait_recording(5)
